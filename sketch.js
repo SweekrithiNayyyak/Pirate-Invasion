ddc5328f62329fc67bfa66a02d59851d6fcae59e
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-06-18 09:31:06 +0530

    teach act 2

--- sketch.js
+++ sketch.js
@@ -9,17 +9,24 @@ var engine, world;
 var ball;
 var wedge;
 var angle=60;
+var wall1,wall2,wall3,wall4;
 function setup() {
   createCanvas(400,400);
   engine=Engine.create();
   world=engine.world;
+
+wall1=new Ground(200,390,400,20);
+wall2= new Ground(390,200,20,400);
+wall3= new Ground(10,200,20,400);
+wall4=new Ground(200,10,400,20);
 var ball_options={
     restitution:1,
     friction:0.01
 }
   ball=Bodies.circle(100,20,40,ball_options);
   World.add(world,ball);
-ground_options={
+
+/*ground_options={
     isStatic:true
 }
   ground=Bodies.rectangle(0,380,600,20,ground_options);
@@ -33,13 +40,20 @@ ground_options={
   
 
   wedge2=Bodies.rectangle(200,200,100,20,ops)
-World.add(world,wedge2)
+World.add(world,wedge2)*/
 }
 
 function draw() 
 {background(0)
     Engine.update(engine);
-    Matter.Body.rotate(wedge,angle)
+
+    wall1.show();
+    wall2.show();
+    wall3.show();
+    wall4.show();
+
+
+   /* Matter.Body.rotate(wedge,angle)
     push()
     translate(wedge.position.x,wedge.position.y)
     rotate(angle)
@@ -47,10 +61,10 @@ function draw()
     rect(0,0,100,20)
     pop()
    angle+=0.05;
-
+*/
   ellipse(ball.position.x,ball.position.y,40)
-  rect(ground.position.x,ground.position.y,600,20);
-  rect(wedge2.position.x,wedge2.position.y,100,20)
+  //rect(ground.position.x,ground.position.y,600,20);
+  //rect(wedge2.position.x,wedge2.position.y,100,20)
   
   
 }
