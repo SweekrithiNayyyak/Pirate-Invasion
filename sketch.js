2dac85106c57db705a954dd7bb48d818de2144fc
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-07-11 12:57:58 +0530

    cannon balls and trajectory

--- sketch.js
+++ sketch.js
@@ -5,43 +5,70 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var canvas, angle, tower, ground, cannon;
 
-var tower;
-
+var cannonBall;
+var balls=[];
 
 function preload() {
- backgroundImg = loadImage("./assets/background.gif");
- // towerImage = loadImage("./assets/tower.png");
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
 
 }
 
 function setup() {
-  canvas = createCanvas(1200,600);
+  canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  tower = new Tower(150,350,160,310);
-  ground=new Ground(600,500,1200,50)
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 110, 50, angle);
  
   
+
 }
 
 function draw() {
-  background(backgroundImg);
-  //image(backgroundImg, 0, 0, width, height);
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
 
   
 
   Engine.update(engine);
- // ground.display();
+  ground.display();
   
-
+    for(i=0;i<balls.length;i++){
+      showCannonBalls(balls[i],i)
+    }
+  cannon.display();
   tower.display();
-  
+ // showCannonBalls();
+  //cannonBall.display();
+  console.log(balls)
+     
  
 }
 
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    console.log("hellow")
+     cannonBall=new CannonBall(cannon.x,cannon.y);
+     balls.push(cannonBall);
+     
+  }
+}
+function showCannonBalls(ball,index){
+  ball.display();
+  if(ball.body.position.x>width || ball.body.position.y>height-50){
+    World.remove(world,ball.body);
+    balls.splice(index,1);
 
+  }
 
+}
 
-
-
-
+function keyReleased(){
+  if(keyCode===DOWN_ARROW){
+    balls[balls.length-1].shoot();
+  }
+  //shoot the cannonballs
+}
